c40e9f3af8429bf6b25e0fc440b51571e8b40606
Alsiev Yusup <isaalsiev@gmail.com>
2025-12-09 14:35:18 +0300

    Закончил транзакцию оплаты и добавил роуты пользователя

--- internal/transport/rout.go
+++ internal/transport/rout.go
@@ -11,12 +11,15 @@ func RegisterRoutes(
 	router *gin.Engine,
 	log *slog.Logger,
 	category service.CategoryServices,
-	plan  service.ExercisePlanServices,
-){
+	plan service.ExercisePlanServices,
+	user service.UserService,
+) {
 
 	categoryHandler := NewCategoryHandler(category, log)
-	planHandler := NewExercisePlanHandler(plan,log)
+	planHandler := NewExercisePlanHandler(plan, log)
+	userHandler := NewUserHandler(user, log)
 
 	categoryHandler.RegisterRoutes(router)
 	planHandler.RegisterRoutes(router)
+	userHandler.UserRoutes(router)
 }
